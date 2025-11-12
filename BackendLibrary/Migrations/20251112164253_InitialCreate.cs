cef1c9c53f3c9c781c5599b1a04859217c39f202
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-11-12 13:46:03 -0300

    feat: Add InitialCreate migration

---
+++ BackendLibrary/Migrations/20251112164253_InitialCreate.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace BackendLibrary.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tb_book",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    title = table.Column<string>(type: "character varying(200)", maxLength: 200, nullable: false),
+                    author = table.Column<string>(type: "character varying(150)", maxLength: 150, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_book", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tb_book_category",
+                columns: table => new
+                {
+                    book_id = table.Column<int>(type: "integer", nullable: false),
+                    category_id = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tb_book_category", x => new { x.book_id, x.category_id });
+                    table.ForeignKey(
+                        name: "FK_tb_book_category_tb_book_book_id",
+                        column: x => x.book_id,
+                        principalTable: "tb_book",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_tb_book_category_tb_category_category_id",
+                        column: x => x.category_id,
+                        principalTable: "tb_category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_tb_book_category_category_id",
+                table: "tb_book_category",
+                column: "category_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tb_book_category");
+
+            migrationBuilder.DropTable(
+                name: "tb_book");
+
+            migrationBuilder.DropTable(
+                name: "tb_category");
+        }
+    }
+}
