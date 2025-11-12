55461a91b9b2e0cf7d65a347a1806941b4f7c1f4
Matheus Valdevino <matheusvaldevino1997@gmail.com>
2025-11-12 13:24:26 -0300

    feat: Add Models (Category, Book)

---
+++ BackendLibrary/Models/Book.cs
@@ -0,0 +1,10 @@
+namespace BackendLibrary.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public ICollection<Category> Categories { get; set; } = [];
+    }
+}
---
+++ BackendLibrary/Models/Category.cs
@@ -0,0 +1,9 @@
+namespace BackendLibrary.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public ICollection<Book> Books { get; set; } = [];
+    }
+}
